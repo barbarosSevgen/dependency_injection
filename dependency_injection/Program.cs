76a1d8a56046ccf572eb28b17bb3b885868208db
Barbaros Sevgen <129516141+barbarosSevgen@users.noreply.github.com>
2023-07-20 09:22:30 +0300

    Program.cs

--- dependency_injection/Program.cs
+++ dependency_injection/Program.cs
@@ -1,49 +1,47 @@
-﻿using System.Buffers.Text;
-
 namespace dependency_injection
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			BaseDatabase baseDatabase = new BaseDatabase(new Oracle());
-			baseDatabase.Connect();
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseDatabase baseDatabase = new BaseDatabase(new Oracle());
+            baseDatabase.Connect();
         }
-	}
+    }
 
-	interface IDatabase
-	{
-		void Connect();
-	}
+    interface IDatabase
+    {
+        void Connect();
+    }
 
-	abstract class Database : IDatabase
-	{
-		protected string? basedatabase;
+    abstract class Database : IDatabase
+    {
+        protected string? basedatabase;
 
         public void Connect()
-		{
-			Console.WriteLine(basedatabase + " " + "Connected.");
-		}
-	}
+        {
+            Console.WriteLine(basedatabase + " " + "Connected.");
+        }
+    }
 
-	class BaseDatabase
-	{
-		IDatabase usingdatabase;
+    class BaseDatabase
+    {
+        IDatabase usingdatabase;
         public BaseDatabase(IDatabase database)
         {
             usingdatabase = database;
         }
         public void Connect()
-		{
+        {
             usingdatabase.Connect();
         }
     }
 
-	class Oracle : Database
-	{
+    class Oracle : Database
+    {
         public Oracle()
         {
-			basedatabase = "Oracle";
+            basedatabase = "Oracle";
         }
     }
 
