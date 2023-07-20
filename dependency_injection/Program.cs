d1491c927877b4bfe16bb5ea72559e064ec6c80a
bsevgen <bsevgen@HEKTASPC307>
2023-07-20 09:17:33 +0300

    Add project files.

---
+++ dependency_injection/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Buffers.Text;
+
+namespace dependency_injection
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			BaseDatabase baseDatabase = new BaseDatabase(new Oracle());
+			baseDatabase.Connect();
+        }
+	}
+
+	interface IDatabase
+	{
+		void Connect();
+	}
+
+	abstract class Database : IDatabase
+	{
+		protected string? basedatabase;
+
+        public void Connect()
+		{
+			Console.WriteLine(basedatabase + " " + "Connected.");
+		}
+	}
+
+	class BaseDatabase
+	{
+		IDatabase usingdatabase;
+        public BaseDatabase(IDatabase database)
+        {
+            usingdatabase = database;
+        }
+        public void Connect()
+		{
+            usingdatabase.Connect();
+        }
+    }
+
+	class Oracle : Database
+	{
+        public Oracle()
+        {
+			basedatabase = "Oracle";
+        }
+    }
+
+    class MSsql : Database
+    {
+        public MSsql()
+        {
+            basedatabase = "MSsql";
+        }
+    }
+}
